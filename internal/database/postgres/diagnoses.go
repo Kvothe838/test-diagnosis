f6174e87777d7dde26ecfb43d81d148487b2967d
Kvothe838 <facundoyuffrida@gmail.com>
2024-06-13 17:43:54 +0200

    Added patient name in query filter for diagnoses

--- internal/database/postgres/diagnoses.go
+++ internal/database/postgres/diagnoses.go
@@ -10,6 +10,7 @@ func (r repository) SearchDiagnoses(ctx context.Context, filters models.SearchDi
 	baseQuery := `
     SELECT d.id, d.patient_id, d.date, d.description, d.prescription 
     FROM diagnoses d
+    JOIN patients p ON d.patient_id = p.id
 	WHERE 1=1
 	`
 	query, args := buildQuery(baseQuery, filters)
@@ -55,6 +56,11 @@ func buildQuery(baseQuery string, filters models.SearchDiagnosesFilters) (string
 	if !filters.Date.IsZero() {
 		query += " AND d.date = :date"
 		args["date"] = filters.Date
+	}
+
+	if len(filters.PatientName) != 0 {
+		query += " AND CONCAT(p.name, ' ', p.surname) LIKE :full_name"
+		args["full_name"] = "%" + filters.PatientName + "%"
 	}
 
 	return query, args
